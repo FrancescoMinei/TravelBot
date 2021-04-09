fc87e84c576bbd45f6ae1712230ba103c93e0994
Francesco Minei <francesco.minei.studenti@isii.it>
2021-04-09 23:29:06 +0200

    async and await fix

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ const db = require('better-sqlite3')('./TravelBot.db', { verbose: console.log })
 const emoji = require('node-emoji');
 const express = require('express')
 
-const WelcomeMsg = 'Benvenuto in  TravelBot, ';
+const WelcomeMsg = 'Benvenuto in TravelBot, ';
 const HelpMsg = 'La ricerca degli hotel va in base al codice IATA, il codice aereoportuale.';
 const View = 'Ecco il dataset con le città';
 const Send = 'Invia il codice di una città';
+const SendC = 'Invia le coordinate di una città (separate da uno spazio)';
 const Search = 'Invia il nome di una città per verificare se contenuta del database';
 
 var Amadeus = require('amadeus');
@@ -33,7 +34,7 @@ bot.onText(/\/view/, msg => {
 
 bot.onText(/\/search/, msg => {
     bot.sendMessage(msg.chat.id, Search).then(() => {
-        bot.on('message', (msg) => {
+        let handler = (msg) => {
             const row = db.prepare('SELECT * FROM City WHERE City.City LIKE ?').all(msg.text.toString());
             let ans = "";
             if (row.length != 0) {
@@ -41,30 +42,35 @@ bot.onText(/\/search/, msg => {
                 bot.sendMessage(msg.chat.id, ans);
             } else
                 bot.sendMessage(msg.chat.id, "Nessuna città trovata");
-            return;
-        });
+            bot.removeListener("message", handler);
+        }
+        bot.on('message', handler);
     });
 });
 
-bot.onText(/\/send/, msg => {
-    let json = new Array;
+bot.onText(/\/code/, msg => {
     bot.sendMessage(msg.chat.id, Send).then(() => {
-        bot.on('message', (msg) => {
+        let handler = (msg) => {
             bot.sendMessage(msg.chat.id, "Sto cercando i migliori hotel...");
             let city = msg.text.toString();
-            console.log(amadeus.shopping.hotelOffers.get({
-                cityCode: city
-            }).then(function(response) {
-                json.push(response.data);
-                return amadeus.next(response);
-            }).then(function(nextResponse) {
-                json.push(nextResponse.data);
-            }).catch(function(error) {
-                console.log(error.code);
-            }));
-            let flag = setTimeout(bot.sendMessage(msg.chat.id, GetName(json).toString()), 2000);
-            return;
-        });
+            GetHotelJsonIata(city, msg.chat.id);
+            bot.removeListener("message", handler);
+        }
+        bot.on('message', handler);
+    });
+});
+
+bot.onText(/\/coordinates/, msg => {
+    bot.sendMessage(msg.chat.id, SendC).then(() => {
+        let handler = (msg) => {
+            bot.sendMessage(msg.chat.id, "Sto cercando i migliori hotel...");
+            let coord = msg.text.toString().split(' ');
+            let lat = coord[0];
+            let lon = coord[1];
+            GetHotelJsonCoordinates(lon, lat, msg.chat.id);
+            bot.removeListener("message", handler);
+        }
+        bot.on('message', handler);
     });
 });
 
@@ -76,4 +82,51 @@ function GetName(json) {
         })
     });
     return data;
+}
+async function GetHotelJsonIata(city, id) {
+    return Promise.resolve('a').then(async function() {
+        let json = new Array;
+        await amadeus.shopping.hotelOffers.get({
+            cityCode: city
+        }).then(function(response) {
+            json.push(response.data);
+            return amadeus.next(response);
+        }).then(function(nextResponse) {
+            json.push(nextResponse.data);
+        }).catch(function(error) {
+            console.log(error.code);
+        });
+        console.log(GetName(json).toString());
+        bot.sendMessage(id, GetName(json).toString());
+    });
+}
+
+async function GetHotelJsonCoordinates(lon, lat, id) {
+    return Promise.resolve('a').then(async function() {
+        let json = new Array;
+        await amadeus.shopping.hotelOffers.get({
+            latitude: lat,
+            longitude: lon
+        }).then(function(response) {
+            json.push(response.data);
+            return amadeus.next(response);
+        }).then(function(nextResponse) {
+            json.push(nextResponse.data);
+        }).catch(function(error) {
+            console.log(error.code);
+        });
+        console.log(GetName(json).toString());
+        bot.sendMessage(id, GetName(json).toString());
+    });
+}
+
+function CheckCoordinate(coord) {
+    let data = coord.split(' ');
+    try {
+        let lat = parseFloat(data[0]);
+        let lon = parseFloat(data[1]);
+
+    } catch (err) {}
+
+
 }
